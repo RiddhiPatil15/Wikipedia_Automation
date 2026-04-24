e85e51ecb651ca704f93784b4c870ee1194dcc0f
Riddhi <140433388+RiddhiPatil15@users.noreply.github.com>
2026-04-24 10:56:55 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,11 +1,10 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.time.Duration;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -17,21 +16,14 @@ public class BaseTest {
 
     public static void initDriver() {
         try {
-            // ✅ Auto download driver
-            WebDriverManager.chromedriver().setup();
-
             ChromeOptions options = new ChromeOptions();
-
-            // ✅ REQUIRED for Jenkins (headless)
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--remote-allow-origins=*");
 
-            driver = new ChromeDriver(options);
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444"),
+                    options
+            );
 
-            // ✅ Stability
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.manage().window().maximize();
 
         } catch (Exception e) {
