51eceb8930a6bf91170541194c0cf923c05ec8e3
Riddhi <140433388+RiddhiPatil15@users.noreply.github.com>
2026-04-24 11:01:41 +0530

    Integrate WebDriverManager for ChromeDriver setup
    Added WebDriverManager setup for ChromeDriver initialization.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
@@ -13,12 +14,13 @@ public class BaseTest {
     }
 
     public static void initDriver() {
+        WebDriverManager.chromedriver().setup();
+    
         ChromeOptions options = new ChromeOptions();
-
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-
+    
         driver = new ChromeDriver(options);
     }
 
