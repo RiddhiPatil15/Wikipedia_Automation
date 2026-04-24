c92e901c8f02c49cb6e344edef411d25a1f8105b
Riddhi <140433388+RiddhiPatil15@users.noreply.github.com>
2026-04-24 11:00:20 +0530

    Refactor BaseTest to use ChromeDriver with options

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,10 +1,8 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.net.URL;
 
 public class BaseTest {
 
@@ -16,19 +14,13 @@ public class BaseTest {
 
     public static void initDriver() {
         ChromeOptions options = new ChromeOptions();
+
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-    
+
         driver = new ChromeDriver(options);
     }
-     public static void quitDriver() {
-            if (driver != null) {
-                driver.quit();
-            }
-        }
-    }
-    
 
     public static void quitDriver() {
         if (driver != null) {
